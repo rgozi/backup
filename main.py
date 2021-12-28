ff93ec71f85b9b83d558e85c9649f0c89fe2072e
iamwm <new_git_wm@163.com>
2021-12-28 17:37:09 +0800

    fix: add ignore file and reset files

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ def new_backup():
     archive_folder = DEFAULT_ARCHIVE_FOLDER
     target_folder = pathlib.Path.joinpath(DEFAULT_BACKUP_FOLDER, current_timestamp)
     shutil.copytree(archive_folder, target_folder)
+    print(f'new backup:{current_timestamp} generated')
 
 
 def list_backup():
@@ -25,7 +26,49 @@ def load_backup(backup_name: str):
     shutil.rmtree(DEFAULT_ARCHIVE_FOLDER)
     target_folder = pathlib.Path.joinpath(DEFAULT_BACKUP_FOLDER, backup_name)
     shutil.copytree(target_folder, DEFAULT_ARCHIVE_FOLDER)
+    print(f'use backup:{backup_name}')
+
+
+def make_choice(min_value: int, max_value: int):
+    while True:
+        try:
+            choice = input(f'input a number from {min_value} to {max_value} to make a choice:')
+            if choice not in range(min_value, max_value + 1):
+                raise Exception(f'unknown choice: {choice}')
+        except Exception as e:
+            print(e)
+        else:
+            return choice
+
+
+def quick_backup():
+    print("make sure you've exit game already")
+    new_backup()
+
+
+def quick_load():
+    print('these are latest backup:')
+    current_backup = list_backup()
+    if not current_backup:
+        print('no available backup')
+        return
+    for backup in current_backup:
+        print(f'[{backup[0]}]:{backup[1]}')
+    choice = make_choice(1, len(current_backup))
+    target_backup = current_backup[choice - 1]
+    load_backup(target_backup[1])
+
+
+def main():
+    print('choose what you want:')
+    print('[1] quick backup')
+    print('[2] load a backup')
+    things_to_do = make_choice(1, 2)
+    if things_to_do == 1:
+        quick_backup()
+    else:
+        quick_load()
 
 
 if __name__ == '__main__':
-    pass
+    main()
